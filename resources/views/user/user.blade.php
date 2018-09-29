e90e7c8812b64f5d75386a5439f424efaafadc5e
webdeveloper0418 <kungfupanda0418@gmail.com>
2018-09-29 17:05:52 +0100

    video-vimeo-player

---
+++ resources/views/user/user.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.userapp')
+
+@section('content')
+
+    <v-container fluid grid-list-md text-xs-center>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title class="blue darken-3 white--text"><h2>User</h2></v-card-title>
+                    <v-fade-transition mode="out-in">
+                        <router-view></router-view>
+                    </v-fade-transition>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+
+@endsection
